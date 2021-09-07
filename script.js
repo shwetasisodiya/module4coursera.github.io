9f2b1c3c60951fa92ccd46ef96539f799d401760
shwetasisodiya <84631711+shwetasisodiya@users.noreply.github.com>
2021-09-07 00:40:05 -0800

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@
 
  for(var i=0; i<names.length; i++ ) {
    if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j') {
-   	console.log("Goodnye "+ names[i])
+   	console.log("Goodbye "+ names[i])
    }
    else {
    	console.log("hello "+ names[i])
